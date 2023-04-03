90ed5f2f6ec217cb718c582b5f77a5d04113322f
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-04-03 21:15:53 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const menuItem=document.getElementById("menuItems")
+menuItem.style.maxHeight="0px";
+
+
+function menuToggle(){
+    if(menuItem.style.maxHeight=="0px"){
+        menuItem.style.maxHeight="200px";
+    }
+    else{
+        menuItem.style.maxHeight="0px"
+    }
+}
+
+
